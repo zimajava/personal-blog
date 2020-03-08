9545175f1131101b15fa6cd639cf46ecef5f9987
Zima Maksim <maksimz@wizardsdev.com>
2020-03-08 15:27:23 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <StaticQuery
       query={graphql`
         {
